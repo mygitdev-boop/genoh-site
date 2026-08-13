960791f562a89b23a2a64983ccb0402580179a8b
Jaiveer <way2class@192.168.0.101>
2026-08-13 21:29:16 +0530

    Set genohin.com (apex) as canonical site URL

--- lib/data/site.ts
+++ lib/data/site.ts
@@ -1,4 +1,4 @@
-export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.genohin.com";
+export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://genohin.com";
 
 export const site = {
   name: "GENOH",
